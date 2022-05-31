388825de5a970bd1f4641e54455fa0295839ca80
Akshat <akshatkool2@gmail.com>
2022-05-31 17:24:06 +0530

    lorem

--- client/src/components/Cancel.jsx
+++ client/src/components/Cancel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Cancel() {
   return (
-    <h1>Cancel</h1>
+    <h1>Cancel Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, quas consequuntur doloremque temporibus quod omnis a provident inventore quasi consequatur neque aut! Impedit earum aut, veritatis cum aliquam temporibus nisi.</h1>
   )
 }
 
